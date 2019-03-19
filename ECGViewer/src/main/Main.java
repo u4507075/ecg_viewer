392ec20381b3d0aad0528c71a0907d172bd92311
Piyapong Khumrin <u4507075@gmail.com>
2019-03-19 15:36:19 +1100

    add project

---
+++ ECGViewer/src/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author bon
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Frame.main(args);
+    }
+}
